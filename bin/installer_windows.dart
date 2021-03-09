8f64d3f760e266c4c920150b8b2c2571940f42d8
Anaïs Betts <anais@anaisbetts.org>
2021-03-10 00:38:20 +0100

    Smush the data from pubspec.yaml into something typed

--- bin/installer_windows.dart
+++ bin/installer_windows.dart
@@ -2,9 +2,10 @@
 
 library installer_windows;
 
-import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:yaml/yaml.dart';
 import 'package:args/args.dart';
 import 'package:path/path.dart' as path;
@@ -19,21 +20,129 @@ final appDir = path.canonicalize(
 
 final pubspecYaml = path.join(appDir, 'pubspec.yaml');
 
+const youDontHaveAProperPubspec = '''
+Your app's pubspec.yaml doesn't have a Squirrel section, it is Highly Recommended 
+to have one! Here's an example section with all of the available parameters, though
+most are optional:
+
+squirrel:
+  windows:
+    certificateFile: "foo"
+    overrideSigningParameters: "bar"
+    loadingGif: "baz"
+    iconUrl: "blahhgh"
+    appFriendlyName: "blaf"
+    appDescription: "blaf"
+    setupIcon: "bamf"
+    releaseDirectory: "blaf"
+    buildEnterpriseMsiPackage: false
+    dontBuildDeltas: false
+''';
+
+String stringOrThrow(dynamic? d, String err) {
+  if (d == null) {
+    throw Exception(err);
+  }
+
+  return d.toString();
+}
+
+String parseVersion(dynamic? v) {
+  final ver = stringOrThrow(v, 'Your app needs a version');
+  return ver.replaceFirst(RegExp(r'[-+].*$'), '');
+}
+
+class PubspecParams {
+  final String name;
+  final String title;
+  final String version;
+  final String authors;
+  final String description;
+  final String? certificateFile;
+  final String? overrideSigningParameters;
+  final String? loadingGif;
+  final String? appIconUrl;
+  final String? setupIcon;
+  final String releaseDirectory;
+  final bool buildEnterpriseMsiPackage;
+  final bool dontBuildDeltas;
+
+  PubspecParams(
+      this.name,
+      this.title,
+      this.version,
+      this.authors,
+      this.description,
+      this.certificateFile,
+      this.overrideSigningParameters,
+      this.loadingGif,
+      this.appIconUrl,
+      this.setupIcon,
+      this.releaseDirectory,
+      this.buildEnterpriseMsiPackage,
+      this.dontBuildDeltas);
+
+  factory PubspecParams.fromYaml(dynamic appPubspec) {
+    dynamic windowsSection = appPubspec['squirrel']['windows'];
+    if (windowsSection == null) {
+      stderr.writeln(youDontHaveAProperPubspec);
+      windowsSection = {};
+    }
+
+    final name = appPubspec['name'].toString();
+    final title = stringOrThrow(
+        windowsSection['appFriendlyName'] ?? appPubspec['title'],
+        'Your app needs a description!');
+    final version = parseVersion(appPubspec['version']);
+    final authors = stringOrThrow(
+        appPubspec['authors'], 'Your pubspec must have an authors section');
+    final description = stringOrThrow(windowsSection['appDescription'] ?? title,
+        'Your app must have a description');
+    final certificateFile = windowsSection['certificateFile']?.toString();
+    final overrideSigningParameters =
+        windowsSection['overrideSigningParameters']?.toString();
+    final loadingGif = windowsSection['loadingGif']?.toString();
+    final appIconUrl = windowsSection['appIconUrl']?.toString();
+    final setupIcon = windowsSection['setupIcon']?.toString();
+    final releaseDirectory = windowsSection['releaseDirectory']?.toString() ??
+        path.join(appDir, 'build');
+    final buildEnterpriseMsiPackage =
+        windowsSection['buildEnterpriseMsiPackage'] == true ? true : false;
+    final dontBuildDeltas =
+        windowsSection['dontBuildDeltas'] == true ? true : false;
+
+    return PubspecParams(
+        name,
+        title,
+        version,
+        authors,
+        description,
+        certificateFile,
+        overrideSigningParameters,
+        loadingGif,
+        appIconUrl,
+        setupIcon,
+        releaseDirectory,
+        buildEnterpriseMsiPackage,
+        dontBuildDeltas);
+  }
+}
+
 Future<int> main(List<String> args) async {
-  final pubspec = loadYaml(await File(pubspecYaml).readAsString());
+  final yaml = loadYaml(await File(pubspecYaml).readAsString());
 
   final template = Environment().fromString(
       await File(path.join(rootDir, 'nuspec.jinja')).readAsString());
 
+  final pubspec = PubspecParams.fromYaml(yaml);
+
   print(template.render(
-      name: pubspec['name'],
-      title: pubspec['title'],
-      version: pubspec['version'],
-      authors: pubspec['authors'] ?? 'FILL IN THE AUTHORS',
-      iconUrl: 'FILL THIS IN',
-      description: 'FILL THIS IN',
-      copyright: 'FILL THIS IN',
-      exe: 'FILL THIS IN',
+      name: pubspec.name,
+      title: pubspec.title,
+      description: pubspec.description,
+      version: pubspec.version,
+      authors: pubspec.authors,
+      iconUrl: pubspec.appIconUrl,
       additionalFiles: <dynamic>[]));
 
   return 0;
